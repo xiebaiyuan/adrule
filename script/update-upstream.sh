f585468a66541a069b5bd241bdd7290fa34f386a
xiebaiyuan <xiebaiyuan@139.com>
2025-08-19 20:38:52 +0800

    update my rules
    Former-commit-id: f8b6c5948e4451ffdecc5ca9ba2c860b1cae2779

--- script/update-upstream.sh
+++ script/update-upstream.sh
@@ -185,7 +185,7 @@ dns=(
   #XingShao AdRules DNS List
   "https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt"
   #catteam (自己的规则)
-  "https://raw.githubusercontent.com/xiebaiyuan/AdGuard-Rule/adrules/dns.txt"
+  #"https://raw.githubusercontent.com/xiebaiyuan/AdGuard-Rule/adrules/dns.txt"
   ### PCDN rules ###
   #fuck pcdn
   "https://thhbdd.github.io/Block-pcdn-domains/ban.txt"
